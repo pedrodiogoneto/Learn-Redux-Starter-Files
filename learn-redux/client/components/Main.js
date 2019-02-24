2796b98c8cb268e0722d010fa5e783bd6440023d
Pedro Neto <pedro.diogo.neto@outlook.pt>
2019-02-24 16:18:10 +0100

    Create main.js

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+function Main() {
+    return (
+        <h1>
+            <Link to={'/'}>Reduxtagram</Link>
+        </h1>
+    )   
+}
+
+export default Main
